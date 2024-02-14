ad7ae41966d09033445a348122432fbc7c23a0f4
Ernesto Serrano <erseco@gmail.com>
2024-02-14 13:35:13 +0000

    Initial version

---
+++ Dockerfile
@@ -0,0 +1,43 @@
+ARG ARCH=
+FROM ${ARCH}erseco/alpine-php-webserver:latest
+
+LABEL maintainer="Ernesto Serrano <info@ernesto.es>"
+
+USER root
+COPY --chown=nobody rootfs/ /
+
+# Install WP-CLI
+RUN curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && \
+    php wp-cli.phar --info && \
+    chmod +x wp-cli.phar && \
+    mv wp-cli.phar /usr/local/bin/wp
+
+USER nobody
+
+# Here we assume that WORDPRESS_VERSION is passed at build time. By default, we use 'latest'.
+ARG WORDPRESS_VERSION=latest
+
+ENV WP_LANGUAGE=en_US \
+    DB_HOST=mariadb \
+    DB_PORT=3306 \
+    DB_NAME=wordpress \
+    DB_USER=wordpress \
+    DB_PASS=wordpress \
+    DB_PREFIX=wp_ \
+    WP_ADMIN_EMAIL=admin@example.com \
+    WP_ADMIN_USERNAME=admin \
+    WP_ADMIN_PASSWORD=PLEASE_CHANGEME \
+    WP_DEBUG=false \
+    WP_CLI_CACHE_DIR=/tmp/wp-cli/cache/ \
+    WP_THEME= \
+    WP_PLUGINS= \
+    WP_SITE_TITLE="WordPress Site" \
+    WP_SITE_DESCRIPTION="Just another WordPress site" \
+    WP_SITE_URL=http://localhost:8080 \
+    client_max_body_size=50M \
+    post_max_size=50M \
+    upload_max_filesize=50M \
+    max_input_vars=5000 
+
+# Use WP-CLI to download WordPress, using the version specified or 'latest' by default.
+RUN wp core download --version=${WORDPRESS_VERSION} --path=/var/www/html
---
+++ rootfs/docker-entrypoint-init.d/02-configure-wordpress.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+#
+# WordPress configuration script
+#
+set -eo pipefail
+
+# Path to WordPress root directory
+wp_root="/var/www/html"
+
+# Function to check the availability of a database
+check_db_availability() {
+    local db_host="$1"
+    local db_port="$2"
+
+    echo "Waiting for $db_host:$db_port to be ready..."
+    while ! nc -w 1 "$db_host" "$db_port"; do
+        # Show some progress
+        echo -n '.'
+        sleep 1
+    done
+    echo -e "\n\nGreat, $db_host is ready!"
+}
+
+# Check the availability of the primary database
+check_db_availability "$DB_HOST" "$DB_PORT"
+
+# Check if wp-config.php exists and create it if not
+if [ ! -f "$wp_root/wp-config.php" ]; then
+    wp config create --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASSWORD" --dbhost="$DB_HOST" --dbprefix="$DB_PREFIX" --path="$wp_root"
+fi
+
+# Install WordPress if not already installed
+if ! wp core is-installed --path="$wp_root"; then
+    wp core install --url="$WP_SITE_URL" --title="$WP_SITE_TITLE" --admin_user="$WP_ADMIN_USERNAME" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --path="$wp_root"
+fi
+
+# Configure site settings
+wp config set WP_DEBUG $WP_DEBUG --raw
+
+wp option update blogname "$WP_SITE_TITLE" --path="$wp_root"
+wp option update blogdescription "$WP_SITE_DESCRIPTION" --path="$wp_root"
+
+if [ "$WP_LANGUAGE" != "en_US" ]; then
+
+    # Install the specified language package
+    wp language core install "$WP_LANGUAGE"
+    # Switch the site language to the specified language
+    wp site switch-language "$WP_LANGUAGE"
+
+fi
+
+# Install and activate theme if WP_THEME is specified
+if [ -n "$WP_THEME" ]; then
+    wp theme install "$WP_THEME" --activate --path="$wp_root"
+fi
+
+# Install and activate plugins if WP_PLUGINS is specified
+if [ -n "$WP_PLUGINS" ]; then
+    OLD_IFS="$IFS"
+    IFS=','
+    for plugin in $WP_PLUGINS; do
+        wp plugin install "$plugin" --activate --path="$wp_root"
+    done
+    IFS="$OLD_IFS"
+fi
+
+echo "WordPress has been successfully configured."
